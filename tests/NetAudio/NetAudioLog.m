e6c735817e3b1baf3db4e04e80bff285c0aa09cd
Anne Heimes <anne.heimes@rwth-aachen.de>
2017-01-26 16:04:23 +0100

    Graphische ausgabe vom Log überarbeitet

--- tests/NetAudio/NetAudioLog.m
+++ tests/NetAudio/NetAudioLog.m
@@ -1,9 +1,34 @@
+%% Einlesen der Logs
 NetAudioLogNet = readtable( 'NetAudioLogNet.txt' );
 NetAudioLogStream = readtable( 'NetAudioLogStream.txt' );
 
-plot( NetAudioLogNet.WorldTimeStamp, NetAudioLogNet.FreeSamples/10240 )
+%% Plot Freie Smaples und Bufferstutus
+minTime = min(NetAudioLogStream.WorldTimeStamp(1),NetAudioLogNet.WorldTimeStamp(1));
+TimeNet = NetAudioLogNet.WorldTimeStamp - minTime;
+TimeStream = NetAudioLogStream.WorldTimeStamp - minTime;
+BlockLenght = NetAudioLogNet.NumSamplesTransmitted(1);
+BufferSize = 100 * BlockLenght;
+Data = [NetAudioLogNet.WorldTimeStamp NetAudioLogNet.FreeSamples; NetAudioLogStream.WorldTimeStamp NetAudioLogStream.FreeSamples];
+Data = sortrows(Data,1 );
+Data(:,1) = Data(:,1) - minTime();
+Data(:,2) = (Data(:,2)) / BlockLenght;
+subplot(2,2,1:2)
+plot(Data(:,1), Data(:,2))
+title('Freie Blöcke im Ring Buffer')
+xlabel('Zeit in s')
+ylabel('Anzahl der Blöcke')
+
+%% Plot Latenz
+subplot(2,2,3:4)
+DiffTime = diff(NetAudioLogNet.WorldTimeStamp * 1000);
+median = mean(DiffTime);
+medianVec = zeros(size(DiffTime)); 
+medianVec = medianVec + median;
+plot( DiffTime, 'b')
 hold on
-plot( NetAudioLogStream.WorldTimeStamp, NetAudioLogStream.FreeSamples/10240 )
-hold on
-%plot( NetAudioLogNet.WorldTimeStamp, NetAudioLogNet.Bufferstatus )
-plot( NetAudioLogStream.WorldTimeStamp, NetAudioLogStream.StreamingStatus )
+plot( medianVec, 'r')
+title('Latenz pro Block')
+legend('Latenz', 'Durchschnittliche Latenz')
+xlabel('Block Nummer')
+ylabel('Zeit in ms')
+legend('show') 
