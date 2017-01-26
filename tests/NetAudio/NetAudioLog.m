33491525380a1b090e027770fda355a0e3bf0d37
Anne <a.heimes93@web.de>
2017-01-26 20:12:08 +0100

    Zeitleisten synchronisiert und streaming status hinzugefügt

--- tests/NetAudio/NetAudioLog.m
+++ tests/NetAudio/NetAudioLog.m
@@ -1,4 +1,6 @@
 %% Einlesen der Logs
+close all;
+clear all;
 NetAudioLogNet = readtable( 'NetAudioLogNet.txt' );
 NetAudioLogStream = readtable( 'NetAudioLogStream.txt' );
 NetAudioLogClient = readtable( 'NetAudioLogClient.txt' );
@@ -8,7 +10,6 @@ minTime = min(NetAudioLogStream.WorldTimeStamp(1),NetAudioLogNet.WorldTimeStamp(
 TimeNet = NetAudioLogNet.WorldTimeStamp - minTime;
 TimeStream = NetAudioLogStream.WorldTimeStamp - minTime;
 BlockLenght = NetAudioLogNet.NumSamplesTransmitted(1);
-BufferSize = 100 * BlockLenght;
 Data = [NetAudioLogNet.WorldTimeStamp NetAudioLogNet.FreeSamples; NetAudioLogStream.WorldTimeStamp NetAudioLogStream.FreeSamples];
 Data = sortrows(Data,1 );
 Data(:,1) = Data(:,1) - minTime();
@@ -19,24 +20,39 @@ title('Freie Blöcke im Ring Buffer')
 xlabel('Zeit in s')
 ylabel('Anzahl der Blöcke')
 
+
+%% Plot Streaming Status
+subplot(2,2,1:2)
+plot( TimeNet, NetAudioLogNet.Bufferstatus ) 
+plot( TimeStream, NetAudioLogStream.StreamingStatus )
+ylim([1.9 3.1])
+title('Streaming Status')
+xlabel('Zeit in ms')
+ylabel('Status')
+
+
 %% Plot Latenz
 subplot(2,2,3:4)
 DiffTime = diff(NetAudioLogClient.WorldTimeStamp * 1000);
 median = mean(DiffTime(:,1));
 medianVec = zeros(size(DiffTime(:,1))); 
 medianVec = medianVec + median;
-DiffTime = [DiffTime NetAudioLogClient.ProtocolStatus(2:end)];
-DiffTime = sortrows(DiffTime,2);
-LatenzWaiting = DiffTime(find(DiffTime(:,2)<222),1);
-LatenzRunnning = DiffTime(find(DiffTime(:,2)>221),1);
-medianRunning = mean(LatenzRunnning(:,1));
+DiffTime = [NetAudioLogClient.WorldTimeStamp(2:end) - minTime DiffTime NetAudioLogClient.ProtocolStatus(2:end)];
+DiffTime = sortrows(DiffTime,3);
+LatenzWaiting = DiffTime(find(DiffTime(:,3)<222), (1:2));
+LatenzRunnning = DiffTime(find(DiffTime(:,3)>221), (1:2));
+medianRunning = mean(LatenzRunnning(:,2));
 medianRunningVec = zeros(size(LatenzRunnning(:,1))); 
 medianRunningVec = medianRunningVec + medianRunning;
-plot( LatenzRunnning, 'b')
+sollLatenz = (BlockLenght / 44100) * 1000;
+sollLatenzVec = zeros(size(LatenzRunnning(:,1))); 
+sollLatenzVec = sollLatenzVec + sollLatenz;
+plot( LatenzRunnning(:,1), LatenzRunnning(:,2))
 hold on
-plot( medianRunningVec, 'r')
+plot( LatenzRunnning(:,1), medianRunningVec, 'r')
+plot( LatenzRunnning(:,1), sollLatenzVec, 'g')
 title('Latenz pro Block')
-legend('Latenz', 'Durchschnittliche Latenz')
-xlabel('Block Nummer')
-ylabel('Zeit in ms')
+legend('Latenz', 'Durchschnittliche Latenz', 'SollLatenz')
+xlabel('Zeit in ms')
+ylabel('Latenz in ms')
 legend('show') 
