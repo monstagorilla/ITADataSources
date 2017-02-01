4c41eaf8b05e6c7c93d42339119a542515d2099a
Anne Heimes <anne.heimes@rwth-aachen.de>
2017-02-01 15:25:46 +0100

    anpassung neustrats mit vershciedenen kanälen

--- tests/NetAudio/ITANetAudioStreamingServerTest.cpp
+++ tests/NetAudio/ITANetAudioStreamingServerTest.cpp
@@ -6,6 +6,8 @@
 #include <ITAStreamFunctionGenerator.h>
 #include <ITAStreamMultiplier1N.h>
 #include <ITAFileDatasource.h>
+#include <VistaBase\VistaTimeUtils.h>
+
 
 using namespace std;
 
@@ -13,27 +15,25 @@ static string g_sServerName = "137.226.61.85";
 static int g_iServerPort = 12480;
 static double g_dSampleRate = 44100;
 static int g_iBlockLength = 32;
-static int g_iChannels = 10;
+static int g_iChannels = 0;
 
 int main( int, char** )
 {
-	std::cout << "BufferSize: " << endl;
-	cin >> g_iBlockLength;
-	cout << "ChannelAnzahl: " << endl;
-	cin >> g_iChannels;
+	while ( true )
+	{
+		g_iChannels++;
+		ITAFileDatasource oFile( "gershwin-mono.wav", g_iBlockLength );
+		oFile.SetIsLooping( true );
+		ITAStreamMultiplier1N oMuliplier( &oFile, g_iChannels );
+		CITANetAudioStreamingServer oStreamingServer;
+		oStreamingServer.SetInputStream( &oMuliplier );
 
+		cout << "Starting net audio server and waiting for connections on '" << g_sServerName << "' on port " << g_iServerPort << endl;
+		oStreamingServer.Start( g_sServerName, g_iServerPort );
+		VistaTimeUtils::Sleep( 11 * 1000 );
 
-	ITAFileDatasource oFile( "gershwin-mono.wav", g_iBlockLength );
-	oFile.SetIsLooping( true );
-	ITAStreamMultiplier1N oMuliplier( &oFile, g_iChannels );
-	CITANetAudioStreamingServer oStreamingServer;
-	oStreamingServer.SetInputStream( &oMuliplier );
-
-	cout << "Starting net audio server and waiting for connections on '" << g_sServerName << "' on port " << g_iServerPort << endl;
-	oStreamingServer.Start( g_sServerName, g_iServerPort );
-
-	int iKey;
-	std::cin >> iKey;
+		oStreamingServer.Stop( );
+	}
 
 	return 0;
 }
