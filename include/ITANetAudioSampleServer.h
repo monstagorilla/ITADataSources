e09426c641690e212daee0c62ac570c4c29fdfe1
Jonas Stienen <jst@akustik.rwth-aachen.de>
2017-04-05 23:31:10 +0200

    Cleanup of NetAudio interface, some more comments in code

--- include/ITANetAudioSampleServer.h
+++ include/ITANetAudioSampleServer.h
@@ -24,11 +24,23 @@
 #include <ITADataSourceRealization.h>
 
 
-//! Sample-generation callback function pointer class
-class CITASampleProcessor : public ITADatasourceRealization
+//! Sample-generation class with abstract method for providing samples
+/*
+ * This ready-to-use class helps to provide samples for a NetAudio streaming server with
+ * a single method for processing that has to be implemented ...
+ * ... just derive and implement Process() method. Have a look at Zero() method
+ * for exemplary usage of sample buffer.
+ */ 
+class ITA_DATA_SOURCES_API CITASampleProcessor : public ITADatasourceRealization
 {
 public:
-	inline CITASampleProcessor( const double dSampleRate, const int iNumChannels, const int iBlockLength )
+	//! Create a sample processor with streaming parameters
+	/*
+	 * @param[in] iNumChannels Channels provided
+	 * @param[in] dSampleRate Audio processing sampling rate
+	 * @param[in] iBlockLength Audio processing block length / buffer size
+	 */ 
+	inline CITASampleProcessor( const int iNumChannels, const double dSampleRate, const int iBlockLength )
 		: ITADatasourceRealization( ( unsigned int ) ( iNumChannels ), dSampleRate, ( unsigned int ) ( iBlockLength ) )
 	{
 		m_vvfSampleBuffer.resize( iNumChannels );
@@ -37,7 +49,6 @@ public:
 
 		Zero();
 	};
-
 
 	//! Sets all channels and samples to zero
 	inline void Zero()
@@ -66,6 +77,7 @@ public:
 	  * to produce or copy audio samples into the internal buffer m_vvfSampleBuffer
 	  *
 	  * @param[in] pStreamInfo Information over streaming status, i.e. sample count and time stamp
+	  *
 	  */
 	virtual void Process( const ITAStreamInfo* pStreamInfo ) =0;
 
@@ -73,7 +85,7 @@ protected:
 	std::vector< std::vector< float > > m_vvfSampleBuffer; //!< Multi-channel sample buffer to be filled
 
 private:
-	//! Delegate internal buffer to audio stream
+	//! Delegate internal buffer to audio stream (ITADatasource)
 	inline void ProcessStream( const ITAStreamInfo* pInfo )
 	{
 		Process( pInfo );
@@ -89,11 +101,11 @@ private:
 	};
 };
 
-//! Network audio sample server (for providing samples via individual callback)
+//! Network audio sample server (for providing samples via derived generator class)
 /**
   * Audio sample transmitter for a networked sample callback function that can connect via TCP/IP.
   *
-  * @sa CITANetAudioStream CITANetAudioStreamingServer
+  * @sa CITANetAudioStream CITANetAudioStreamingServer CITASampleProcessor
   * @note not thread-safe
   */
 class ITA_DATA_SOURCES_API CITANetAudioSampleServer : public CITANetAudioStreamingServer
@@ -123,6 +135,5 @@ private:
 
 	CITASampleProcessor* m_pSampleProcessor; //!< Callback / sample processor
 };
-
 
 #endif // INCLUDE_WATCHER_ITA_NET_AUDIO_SAMPLE_SERVER
