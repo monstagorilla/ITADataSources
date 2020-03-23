3dc63a4505bd1a4cba46968b770b2bdd2cd52fa5
Dipl.-Ing. Jonas Stienen <jst@akustik.rwth-aachen.de>
2020-03-23 15:22:53 +0100

    Extending license

--- include/ITAFileDataSource.h
+++ include/ITAFileDataSource.h
@@ -3,7 +3,7 @@
  *
  *		ITA core libs
  *		(c) Copyright Institute of Technical Acoustics (ITA)
- *		RWTH Aachen University, Germany, 2015-2019
+ *		RWTH Aachen University, Germany, 2015-2020
  *
  * ----------------------------------------------------------------
  *				    ____  __________  _______
